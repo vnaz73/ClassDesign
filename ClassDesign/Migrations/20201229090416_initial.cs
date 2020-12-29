89782e83c67b6741811815d533141a6ae0b90a18
v_n_z list.ru <v_n_z@list.ru>
2020-12-29 12:33:11 +0300

    Добавьте файлы проекта.

---
+++ ClassDesign/Migrations/20201229090416_initial.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ClassDesign.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ClassStandards",
+                columns: table => new
+                {
+                    ClassId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ClassName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ClassStandards", x => x.ClassId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Courses",
+                columns: table => new
+                {
+                    CourseId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CourseName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Courses", x => x.CourseId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Students",
+                columns: table => new
+                {
+                    StudentId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    StudentName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Gender = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ClassId = table.Column<int>(type: "int", nullable: false),
+                    ClassStandardClassId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Students", x => x.StudentId);
+                    table.ForeignKey(
+                        name: "FK_Students_ClassStandards_ClassStandardClassId",
+                        column: x => x.ClassStandardClassId,
+                        principalTable: "ClassStandards",
+                        principalColumn: "ClassId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Exams",
+                columns: table => new
+                {
+                    ExamId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    StudentId = table.Column<int>(type: "int", nullable: false),
+                    CourseId = table.Column<int>(type: "int", nullable: false),
+                    DOE = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Marks = table.Column<double>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Exams", x => x.ExamId);
+                    table.ForeignKey(
+                        name: "FK_Exams_Courses_CourseId",
+                        column: x => x.CourseId,
+                        principalTable: "Courses",
+                        principalColumn: "CourseId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Exams_Students_StudentId",
+                        column: x => x.StudentId,
+                        principalTable: "Students",
+                        principalColumn: "StudentId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Student_Courses",
+                columns: table => new
+                {
+                    SCid = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    StudentId = table.Column<int>(type: "int", nullable: false),
+                    CourseId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Student_Courses", x => x.SCid);
+                    table.ForeignKey(
+                        name: "FK_Student_Courses_Courses_CourseId",
+                        column: x => x.CourseId,
+                        principalTable: "Courses",
+                        principalColumn: "CourseId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Student_Courses_Students_StudentId",
+                        column: x => x.StudentId,
+                        principalTable: "Students",
+                        principalColumn: "StudentId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Exams_CourseId",
+                table: "Exams",
+                column: "CourseId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Exams_StudentId",
+                table: "Exams",
+                column: "StudentId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Student_Courses_CourseId",
+                table: "Student_Courses",
+                column: "CourseId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Student_Courses_StudentId",
+                table: "Student_Courses",
+                column: "StudentId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Students_ClassStandardClassId",
+                table: "Students",
+                column: "ClassStandardClassId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Exams");
+
+            migrationBuilder.DropTable(
+                name: "Student_Courses");
+
+            migrationBuilder.DropTable(
+                name: "Courses");
+
+            migrationBuilder.DropTable(
+                name: "Students");
+
+            migrationBuilder.DropTable(
+                name: "ClassStandards");
+        }
+    }
+}
---
+++ ClassDesign/Program.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace EFCoreClassDesignTechEg
+{
+    class ClassStandard
+    {
+        [Key]
+        public int ClassId { get; set; }
+        public string ClassName { get; set; }
+        public string Description { get; set; }
+        public List<Student> Students { get; set; }
+    }
+    class Student
+    {
+        [Key]
+        public int StudentId { get; set; }
+        public string StudentName { get; set; }
+        public string Gender { get; set; }
+        public int ClassId { get; set; }
+        public ClassStandard ClassStandard { get; set; }
+        public List<Student_Course> Student_Courses { get; set; }
+        public List<Exam> Exams { get; set; }
+    }
+    class Course
+    {
+        [Key]
+        public int CourseId { get; set; }
+        public string CourseName { get; set; }
+        public string Description { get; set; }
+        public List<Student_Course> Student_Courses { get; set; }
+        public List<Exam> Exams { get; set; }
+    }
+    class Student_Course
+    {
+        [Key]
+        public int SCid { get; set; }
+        public int StudentId { get; set; }
+        public int CourseId { get; set; }
+        public Student Student { get; set; }
+        public Course Course { get; set; }
+    }
+    class Exam
+    {
+        [Key]
+        public int ExamId { get; set; }
+        public int StudentId { get; set; }
+        public int CourseId { get; set; }
+        public DateTime DOE { get; set; }
+        public double Marks { get; set; }
+        public Student Student { get; set; }
+        public Course Course { get; set; }
+    }
+    class SchoolDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=localhost;Database=Organization;Trusted_Connection=True;");
+        }
+
+        public DbSet<ClassStandard> ClassStandards { get; set; }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Student_Course> Student_Courses { get; set; }
+        public DbSet<Exam> Exams { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
+
